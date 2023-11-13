382173f63103629899ef4ea8afbbc37284bc9bdd
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-13 12:42:29 +0300

    Написана еще одна реализация интерфейса рекурсивной кривой.

--- sierpinskicurve.cpp
+++ sierpinskicurve.cpp
@@ -50,6 +50,36 @@ void SierpinskiCurve::typeA(short n)
     }
 }
 
+void SierpinskiCurve::typeB(short n)
+{
+    if (n > 0) {
+        typeB(n-1); // возможен переход в этот же тип
+        // запоминаем линию по диагонали вниз и влево 🡧
+        currentPos = nextPos;
+        nextPos.setX(nextPos.x() - lineLenght/(sqrt(2)));
+        nextPos.setY(nextPos.y() + lineLenght/(sqrt(2)));
+        lines.append(QLineF(currentPos, nextPos));
+        emit newLineReady(lines.dequeue()); // сигнализируем о готовности рисовать
+        thread()->sleep(pause); // есть задержка для наглядности
+        typeC(n-1); // возможен переход в тип C
+        // запоминаем линию по вертикали вниз ↓
+        currentPos = nextPos;
+        nextPos.setY(nextPos.y() + lineLenght * 2);
+        lines.append(QLineF(currentPos, nextPos));
+        emit newLineReady(lines.dequeue()); // сигнализируем о готовности рисовать
+        thread()->sleep(pause); // есть задержка для наглядности
+        typeA(n-1); // возможен переход в тип A
+        // запоминаем линию по диагонали вниз и вправо 🡦
+        currentPos = nextPos;
+        nextPos.setX(nextPos.x() + lineLenght/(sqrt(2)));
+        nextPos.setY(nextPos.y() + lineLenght/(sqrt(2)));
+        lines.append(QLineF(currentPos, nextPos));
+        emit newLineReady(lines.dequeue()); // сигнализируем о готовности рисовать
+        thread()->sleep(pause); // есть задержка для наглядности
+        typeB(n-1); // возможен переход в тип B
+    }
+}
+
 SierpinskiCurve::SierpinskiCurve(QObject *parent)
     : QObject{parent}
 {
