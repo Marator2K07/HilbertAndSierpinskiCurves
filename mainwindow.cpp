bcb8a06c2ecac265a25394a699e8094253c92903
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-15 08:29:05 +0300

    Более полная инициализация в конструторе основной формы.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,20 +14,30 @@ MainWindow::MainWindow(QWidget *parent)
     // инициализируем поля
     drawingField = ui->graphicsView; // сделаем ссылку на поле отображения без посредников
     hilbertCurve = new HilbertCurve;
-    hilbertCurve->setN(ui->curveOrderValue->value());
-    hilbertCurve->setInitialLenght(ui->initialCurveLenghtValue->value());
+    sierpinskiCurve = new SierpinskiCurve;
+     // о умолчанию выбрана Гильбертова кривая
+    currentCurve = hilbertCurve;
+    currentCurve->changeN(ui->curveOrderValue->value());
+    currentCurve->changeInitialLenght(ui->initialCurveLenghtValue->value());
     // при каждом новом элементе кривой он будет появляться на представлении
     connect(hilbertCurve, SIGNAL(newLineReady(QLine)),
             drawingField, SLOT(drawLine(QLine)));
-    // связываем спинБоксы со слотами Гильбертовой кривой
+    connect(sierpinskiCurve, SIGNAL(newLineFReady(QLineF)),
+            drawingField, SLOT(drawFLine(QLineF)));
+    // связываем спинБоксы со слотами кривых
     connect(ui->curveOrderValue, SIGNAL(valueChanged(int)),
             hilbertCurve, SLOT(changeN(int)));
+    connect(ui->curveOrderValue, SIGNAL(valueChanged(int)),
+            sierpinskiCurve, SLOT(changeN(int)));
     connect(ui->initialCurveLenghtValue, SIGNAL(valueChanged(int)),
             hilbertCurve, SLOT(changeInitialLenght(int)));
+    connect(ui->initialCurveLenghtValue, SIGNAL(valueChanged(int)),
+            sierpinskiCurve, SLOT(changeInitialLenght(int)));
     // вычисление кривой происходит в отдельном потоке, причем
     // прорисовка идет постепенно, а не сразу, по нажатию кнопки
     QThread *threadAnother = new QThread(this);
     hilbertCurve->moveToThread(threadAnother);
+    sierpinskiCurve->moveToThread(threadAnother);
     connect(threadAnother, SIGNAL(started()),
             hilbertCurve, SLOT(makeCalculation()));
     connect(hilbertCurve, SIGNAL(endBuildCurve()),
@@ -42,4 +52,3 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
